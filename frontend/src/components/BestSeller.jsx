32dd7b2c9ccf93e1b644ecb22b435ee550efa8a1
Aditya Pratap Singh <adityapratap1901@gmail.com>
2026-06-10 22:37:47 +0530

    Added homepage components and context setup

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,42 @@
+import React from 'react'   
+import { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+import { useState, useEffect } from 'react';
+import ProductItem from './ProductItem';
+
+
+
+
+const BestSeller = () => {
+    const{products} = useContext(ShopContext);
+    const [bestSeller, setBestSeller] = useState([]);
+
+    useEffect(() => {
+        const bestProducts = products.filter((item) => (item.bestseller === true));
+        setBestSeller(bestProducts.slice(0, 5));
+    }, [])
+        
+        
+        return (
+        <div className='my-10'>
+            <div className='text-center py-8 text-3xl'>
+                <Title text1={'Best '}  text2={' Sellers'}/>
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-500'>
+                    Explore our top-selling products that customers love
+                </p>
+            </div>
+
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 '>
+                {bestSeller.map((item,index)=>(
+                    <ProductItem key={index} id= {item._id} name={item.name} image={item.image} price={item.price}/> 
+                ))
+                }
+            </div>
+
+        </div>
+    )
+}
+
+
+export default BestSeller
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Hero from '../components/hero'
+import Latestcollection from '../components/latestcollection'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {  
     return (
         <div>
-            
+            <Hero/>
+            <Latestcollection/>
+            <BestSeller/>
         </div>
     )
 }
